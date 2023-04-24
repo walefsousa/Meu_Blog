ebfddbbe93a8db4f003de0018c6cbc0967546336
Walef <walefsouza05@gmail.com>
2023-04-24 07:36:02 -0300

    Inicio projeto Blog

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    
+    include("helps/url.php"); 
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Blog</title>
+    <!-- Estilo do projeto --->
+    <link rel="stylesheet"href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- Fontes do projeto ---> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Olá mundo</h1>
+</body>
+</html>
